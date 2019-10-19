8a0d69cb26bb6149f933896a7197e07becb116a6
Karina <47184215+KarinaSakuta@users.noreply.github.com>
2019-10-19 17:31:10 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const brackets = str.split('');
+
+    let openingBrackets = bracketsConfig.map((bracketsPair) => bracketsPair[0]);
+    let closingBrackets = bracketsConfig.map((bracketsPair) => bracketsPair[1]);
+    let bothSidesBrackets = openingBrackets.filter((bracket) => closingBrackets.includes(bracket));
+
+    openingBrackets = openingBrackets.filter((bracket) => !bothSidesBrackets.includes(bracket));
+    closingBrackets = closingBrackets.filter((bracket) => !bothSidesBrackets.includes(bracket));
+
+    let openingBracketByClosing = {};
+    bracketsConfig.forEach(function (bracketsPair) {
+        openingBracketByClosing[bracketsPair[1]] = bracketsPair[0];
+    });
+
+    let hasErrors = false;
+    const bracketsStack = [];
+
+    for(let index = 0; index <= brackets.length - 1; index++) {
+        const currentBracket = brackets[index];
+        const isBothSidesBracket = bothSidesBrackets.includes(currentBracket);
+        const isOpeningBracket = openingBrackets.includes(currentBracket);
+        const isClosingBracket = closingBrackets.includes(currentBracket);
+        const isMatchingOpeningBracketInStack = bracketsStack[bracketsStack.length - 1] === openingBracketByClosing[currentBracket];
+
+        if (isOpeningBracket || isBothSidesBracket && !isMatchingOpeningBracketInStack) {
+            bracketsStack.push(currentBracket);
+            continue;
+        }
+
+        if (isClosingBracket && isMatchingOpeningBracketInStack || isBothSidesBracket && isMatchingOpeningBracketInStack) {
+            bracketsStack.splice(bracketsStack.length - 1);
+            continue
+        }
+
+        hasErrors = true;
+        break;
+    }
+
+    return bracketsStack.length === 0 && !hasErrors;
 }
